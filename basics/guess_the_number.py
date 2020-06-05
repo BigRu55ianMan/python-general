a13ceb6bc97e3a716d13e960adb6e780a75975db
BigRu55ianMan <66446357+BigRu55ianMan@users.noreply.github.com>
2020-06-05 13:27:24 -0500

    Create guess_the_number.py

---
+++ basics/guess_the_number.py
@@ -0,0 +1,54 @@
+import random
+import pyinputplus
+import pyperclip
+
+pyperclip.copy('python3 guess_the_number.py')
+
+print()
+
+rnum = random.randint(1, 20)
+name = input('Enter your name: ')
+print(rnum)
+
+print()
+
+print(name + ', I have a number between 1 and 20 in my mind, would you like to guess it?')
+response = pyinputplus.inputYesNo('Please reply with a yes or no: ')
+
+print()
+
+if response == 'no':
+    print('Very well then. The number was', rnum, 'by the way.')
+    print()
+    print('Have a wonderfull day!')
+    print()
+    quit()
+elif response == 'yes':
+    print('Feeling lucky today huh. Very well then, you will have 5 guesses')
+
+print()
+
+for guessAttempts in range(0,5):
+    guess = pyinputplus.inputNum('Please, take a guess: ')
+    if guess > rnum:
+        print('Unfortunately, your guess was too high')
+        print()
+    elif guess < rnum:
+        print('Unfortunately, your guess was too low')
+        print()
+    else:
+        print()
+        break
+
+if guess == rnum and guessAttempts == 0:
+    print('Congratulations', name + '! You guessed the number I had in mind in 1 guess! Very impressive.')
+    print()
+elif guess == rnum:
+    print('Congratulations', name + '! You guessed the number I had in mind in', guessAttempts, 'guesses! Impressive.')
+    print()
+else:
+    print('Dear', name + ', it is with great concern and regret that I have to inform you that you failed to guees the number I had in mind.')
+    print()
+    print('The number I had in mind was:', rnum,)
+    print('While your guess was:', guess)
+    print()

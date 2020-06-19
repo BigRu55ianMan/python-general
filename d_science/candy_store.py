3fc4d1ea0482301b83abc65d73bfb167bc300de8
BigRu55ianMan <66446357+BigRu55ianMan@users.noreply.github.com>
2020-06-19 12:48:16 -0500

    Update candy_store.py

---
+++ d_science/candy_store.py
@@ -0,0 +1,32 @@
+import pyinputplus
+
+candy_list = ["Snickers", "Kit Kat", "Sour Patch Kids", "Juicy Fruit", "Swedish Fish", "Skittles", "Hershey Bar", "Starbursts", "M&Ms"]
+candy_cart = []
+
+print()
+
+print('Here is the candy you can buy: \n')
+
+for i in candy_list:
+    print('[' + str(candy_list.index(i)) + ']', i)
+print()
+
+while True:
+    allowance = int(input('What is your allowance? '))
+    if allowance < 1:
+        print('Sorry, you cannot spend less than $1. Please try again. \n')
+    else:
+        break
+        print()
+
+
+for k in range(0, allowance):
+    choice = int(input('Please select a candy to add to your cart: '))
+    print(candy_list[choice], 'was added to your cart \n')
+    candy_cart.append(candy_list[choice])
+
+print('Here is your cart: \n')
+
+for h in candy_cart:
+    print(h)
+print()

c0c9039b5ef8e3ea4c3892959b6cad798916f775
BigRu55ianMan <66446357+BigRu55ianMan@users.noreply.github.com>
2020-06-19 13:21:33 -0500

    Update candy_store.py

--- basics/candy_store.py
+++ basics/candy_store.py
@@ -10,17 +10,16 @@ for i in candy_list:
 print()
 
 while True:
-    allowance = int(input('What is your allowance? '))
+    allowance = int(pyinputplus.inputInt('What is your allowance? '))
     print()
     if allowance < 1:
         print('\nSorry, you cannot spend less than $1. Please try again.\n')
     else:
-        print()
         break
 
 for k in range(0, allowance):
     while True:
-        choice = int(input('Please select a candy to add to your cart: '))
+        choice = int(pyinputplus.inputInt('Please select a candy to add to your cart: '))
         if choice > 8 or choice < 0:
             print('Sorry, the index you entered does not seem to exist in our database. Please try again.\n')
         else:

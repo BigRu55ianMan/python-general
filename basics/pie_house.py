184cd67f8968f39fb25f7bbc3a3140fe661bb71e
BigRu55ianMan <66446357+BigRu55ianMan@users.noreply.github.com>
2020-06-19 20:18:24 -0500

    Update pie_house.py

--- basics/pie_house.py
+++ basics/pie_house.py
@@ -3,7 +3,7 @@ import pyinputplus
 pieList = ['Pecan', 'Apple Crisp', 'Bean', 'Banofee', 'Black Bun', 'Blueberry', 'Buko', 'Burek', 'Tamale', 'Chocolate']
 pieNum = [0] * len(pieList)
 welcome = '\nWelcome to the House of Pies! Here are our pies and their respective indexes:'
-theList = ', '.join('({}) {}'.format(index + 1, element) for index, element in enumerate(pieList)) + '.\n'
+theList = ', '.join('({}) {}'.format(index + 1, element) for index, element in enumerate(pieList)) + '.'
 
 print(welcome)
 print('-' * (len(welcome)-1),'\n')
@@ -11,7 +11,7 @@ print(theList)
 
 while True:
     while True:
-        userChoice = int(pyinputplus.inputInt('Please select your pie using the pie\'s index: '))
+        userChoice = int(pyinputplus.inputInt('\nPlease select your pie using the pie\'s index: '))
         if userChoice < 1 or userChoice > len(pieList):
             print(userChoice, 'is not a valid index. Please try again.\n')
         else:
@@ -20,7 +20,6 @@ while True:
     pieNum[userChoice - 1] += 1
     print('Great! We\'ll have that ' + pieList[userChoice - 1] + ' pie right out for you.')
     response = pyinputplus.inputYesNo('\nWould you like to make another order? ')
-    print()
     if response == 'no':
         break
 

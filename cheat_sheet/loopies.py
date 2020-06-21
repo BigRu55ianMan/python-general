c44a125bacc12714a7d7d39890175ffb15cc506e
BigRu55ianMan <66446357+BigRu55ianMan@users.noreply.github.com>
2020-06-21 12:18:32 -0500

    Update loopies.py

--- cheat_sheet/loopies.py
+++ cheat_sheet/loopies.py
@@ -2,20 +2,28 @@
 while True:
     print('Hello World')
     break
-print()
+print('\n')
 
-#for loops iterate the given variable (i) through the values of a list (numbers), or a list-like value (range(0,3))
+#for loops iterate the given variable (i) through the values of a list (numbers), or a list-like value (range(0,3)).
 numbers = [0, 1, 2]
 
 for i in numbers:
     print(i)
-print()
+print('\n')
 
 for i in range(0,3):
     print(i)
-print()
+print('\n')
 
-#this for loop will print in a line, with the end= parameter being the string to be placed in between the variables
+#this for loop will print in a line, with the end= parameter being the string to be placed in between the variables.
 for i in numbers:
     print(i, end=' ')
-print()
+print('\n\n')
+
+#for loops can iterate more than one variable through the values of thier respective lists usnig the zip() function.
+words = ['banana', 'apple', 'watermelon']
+numbers = [0, 1, 2]
+
+for n, w in zip(numbers, words):
+    print('I have {} {}s'.format(n,w))
+print('\n')

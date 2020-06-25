a3232aa2d8e1f367d0e8990f7e81f754e1dc314e
BigRu55ianMan <66446357+BigRu55ianMan@users.noreply.github.com>
2020-06-25 17:15:19 -0500

    Update encription.py

--- basics/encription.py
+++ basics/encription.py
@@ -4,13 +4,11 @@ import pyinputplus
 import pyperclip
 import re
 
-alphabet = ['A','a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g',
-'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o',
-'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w',
-'X', 'x', 'Y', 'y', 'Z', 'z', '!', '?', ',', '.', ' ', '1', '2', '3', '4', '5',
-'6', '7', '8', '9', '0', '-', '=', '@', '#', '$', '%', '^', '&', '*', '(', ')',
-'_', '+', '~', '`', '[', ']', '|', '{', '}', ';', "'", ':', '"', '<', '>', '/',
-'\\']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+ 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '!', '?', ',', '.',
+ ' ', '1', '2', '3', '4', '5','6', '7', '8', '9', '0', '-', '=', '@', '#', '$',
+ '%', '^', '&', '*', '(', ')','_', '+', '~', '`', '[', ']', '|', '{', '}', ';',
+ "'", ':', '"', '<', '>', '/','\\']
 alphabetS = []
 alphabetD = [0] * len(alphabet)
 
@@ -42,20 +40,18 @@ def decryptor(key):
     global messageD
 
     listKey = key[:key.find('.')]
-    print(listKey)
     messageKey = key[key.find('.')+1:]
-    print(messageKey)
 
     m, l = 0, 0
     k, g = 2, 2
 
     for i in range(int(len(listKey)/2)):
-        alphabetD[int(listKey[l:g])-10] = alphabet[i]
+        alphabetD[int(key[l:g])-10] = alphabet[i]
         l += 2
         g += 2
 
     for i in range(int(len(messageKey)/2)):
-        messageD += alphabetD[int(messageKey[m:k])-10]
+        messageD += str(alphabetD[int(messageKey[m:k])-10])
         m += 2
         k += 2
 
@@ -63,18 +59,17 @@ def messageEncryptor(text):
     global messageToken
     for i in text:
         messageToken += str(alphabetS.index(i) + 10)
+    print('Fantastic. Your encryption code is: {}.{}\n\nYour code was also saved into your clipboard.\nThank you for using my services.\n'.format(listToken, messageToken))
+    pyperclip.copy('{}.{}'.format(listToken, messageToken))
 
 print(greeting)
-userinput = pyinputplus.inputChoice(choices, '\nWould you like to encrypt or decrypt? ')
+userinput = pyinputplus.inputChoice(choices, '\nWould you like to encrypt or decrypt a message? ')
 
 if userinput == 'e' or userinput == 'encrypt' or userinput == 'encryption':
     print('You have selected: Encryption.')
     libraryRandomizer()
     messageEncryptor(input('\nPlease enter the text that you would like to Encript: '))
-    print('Fantastic. Your encryption code is: {}.{}\n\nYour code was also saved into your clipboard.\nThank you for using my services.\n'.format(listToken, messageToken))
-    pyperclip.copy('{}.{}'.format(listToken, messageToken))
 else:
     print('You have selected: Decryption.')
-    userKey = input('\nPlease enter your key: ')
+    userKey = input('\nPlease enter your key: ').lower()
     decryptor(userKey)
-    print('The decrypted message is:\n{}'.format(messageD))

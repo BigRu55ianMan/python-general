f2ed88d94fe89331ddab8076a5f4b225b1e95ba3
BigRu55ianMan <66446357+BigRu55ianMan@users.noreply.github.com>
2020-06-25 20:14:31 -0500

    Update encription.py

--- basics/encription.py
+++ basics/encription.py
@@ -8,7 +8,7 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
  'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '!', '?', ',', '.',
  ' ', '1', '2', '3', '4', '5','6', '7', '8', '9', '0', '-', '=', '@', '#', '$',
  '%', '^', '&', '*', '(', ')','_', '+', '~', '`', '[', ']', '|', '{', '}', ';',
- "'", ':', '"', '<', '>', '/','\\']
+ "'", ':', '"', '<', '>', '/','\\','\t']
 alphabetS = []
 alphabetD = [0] * len(alphabet)
 
@@ -16,7 +16,7 @@ messageToken = ''
 listToken = ''
 messageD = ''
 
-greeting = '\nWelcome to the greatest, safest ecnryptor known to me!\nI provide two services: encryption and decryption.'
+greeting = '\nWelcome to the greatest, safest encryptor known to me!\nI provide two services: encryption and decryption.'
 choices = ['decryption', 'decrypt', 'd', 'encrypt', 'encryption', 'e']
 
 def libraryCheck(text):
@@ -59,16 +59,32 @@ def messageEncryptor(text):
     global messageToken
     for i in text:
         messageToken += str(alphabetS.index(i) + 10)
-    print('Fantastic. Your encryption code is: {}.{}\n\nYour code was also saved into your clipboard.\nThank you for using my services.\n'.format(listToken, messageToken))
     pyperclip.copy('{}.{}'.format(listToken, messageToken))
+
+    print('\nChecking message.\n')
+    time.sleep(1)
+    print('Randomizing libraries.\n')
+    time.sleep(1)
+    print('Generating tokens.\n')
+    time.sleep(1)
+    print('Your code is: {}.{}\n'.format(listToken, messageToken))
+    print('Your encrypted message code has been saved to your clipboard. Do not loose it!\nThank you for using my services.\n')
+
+
 
 print(greeting)
 userinput = pyinputplus.inputChoice(choices, '\nWould you like to encrypt or decrypt a message? ')
 
 if userinput == 'e' or userinput == 'encrypt' or userinput == 'encryption':
-    print('You have selected: Encryption.')
+    print('''You have selected: Encryption.
+
+    Here are some rules that you must remember:
+    1. Only lowercase letters can be decrypted, you can still have capital letters, but they will be saved as lowercase.
+    2. Symbols outside of the standard US keyboard layout cannot be used.''')
+
+    userMessage = input('\nPlease enter the text that you would like to Encript: ').lower()
     libraryRandomizer()
-    messageEncryptor(input('\nPlease enter the text that you would like to Encript: '))
+    messageEncryptor(userMessage)
 else:
     print('You have selected: Decryption.')
     userKey = input('\nPlease enter your key: ').lower()
